48ad2c62286033b7ab22fead691786d37760bcd6
egecinar0 <74179917+egecinar0@users.noreply.github.com>
2021-11-22 22:04:15 +0300

    javaScript

---
+++ Day1/src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+dolarDun = "ankara"
+let dolarBugun = 9.30
+
+console.log(dolarDun)
+
+//const euroDun =  11.2
+//euroDun = 11
+//console.log(euroDun)
+//const değiştirilemediği için hata verir
+
+//array=>dizi
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi" , "Emlak Konut Kredisi" , "Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
+console.log(konutKredileri)
